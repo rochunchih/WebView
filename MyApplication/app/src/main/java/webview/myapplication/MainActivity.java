4d2842fc19c3bd22e628a65fb8f9e4e4bbfde6e1
rochunchih <rochunchih@gmail.com>
2017-12-25 00:35:42 +0800

    first ver.

---
+++ MyApplication/app/src/main/java/webview/myapplication/MainActivity.java
@@ -0,0 +1,17 @@
+package webview.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView myWebView = (WebView) findViewById(R.id.webview);
+        myWebView.loadUrl("https://developer.android.com/guide/webapps/webview.html");
+    }
+}
